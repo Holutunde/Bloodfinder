d4e45a9ab93023b53a05b56ec463264effee6eb8
Holutunde <Holutunde99@gmail.com>
2022-02-17 12:47:56 +0100

    chore(setup): Setup project structure

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 
-export default function App() {
+import { StyleSheet, Text, View } from 'react-native'
+
+const customFonts = {
+  DMSansRegular: require('./assets/fonts/DMSans-Regular.ttf'),
+  DMSansBold: require('./assets/fonts/DMSans-Bold.ttf'),
+  DMSansBoldItalic: require('./assets/fonts/DMSans-BoldItalic.ttf'),
+  DMSansMeduim: require('./assets/fonts/DMSans-Medium.ttf'),
+  DMSansMediumItalic: require('./assets/fonts/DMSans-MediumItalic.ttf'),
+  DMSansItalic: require('./assets/fonts/DMSans-Italic.ttf'),
+}
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +29,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
